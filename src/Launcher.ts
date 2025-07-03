3c735c2698e159ac99481c3258340b3a8cb8a183
Del <aelbanna3896@gmail.com>
2025-07-03 00:02:34 -0400

    Checkpoint: modify Launcher to use new FluidEngine API

--- src/Launcher.ts
+++ src/Launcher.ts
@@ -1,20 +1,14 @@
-import { CoreRuntime } from "@fluid/core/CoreRuntime";
-import { FluidCore } from "@fluid/impl/core/FluidCore";
+import { Fluid } from "fluidengine";
 
-// import { CoreRuntime } from "../../FluidEngine/src/core/CoreRuntime";
 console.log(`Launcher has started!`);
 
 async function main() {
-    console.log(`Beginning initialization sequence...`);
     try {
         console.log("Bootstrapping Fluid Core...");
-        const coreInstance = FluidCore.bootstrap();
-        console.log(`Fluid Core has been instantiated!`, coreInstance);
-        console.log(`Initializing 'CoreRuntime'...`);
-        CoreRuntime.initialize(coreInstance);
-        console.log(`'CoreRuntime' has been initialized!`);
+        const coreInstance = Fluid.bootstrap();
+        console.log(`Fluid Core has been initialized!`, coreInstance);
     } catch (err) {
-        console.error("Core initialization failed:", err);
+        console.error("Fluid Core initialization failed:", err);
         return;
     }
 
