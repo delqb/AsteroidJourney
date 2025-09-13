5b095b40722499482e2c9be791fcd838a68956ad
Del <aelbanna3896@gmail.com>
2025-09-13 18:16:27 -0400

    Move game assets into the src directory and update vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    root: "./src",
     build: {
         target: "esnext",
         minify: false
