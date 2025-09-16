e06f6cace07a840335cf52a5c8cc78d77597b401
Del <aelbanna3896@gmail.com>
2025-09-16 00:49:49 -0400

    Remove output nullification in deploy script

--- scripts/deploy.cmd
+++ scripts/deploy.cmd
@@ -6,14 +6,16 @@ ECHO.
 
 PUSHD %ROOT%
 
-robocopy "%out_directory%" "%temp_out_directory%" /MIR > nul
-CALL :check-error "Failed to mirror copy output directory to deployment staging directory."
+robocopy "%out_directory%" "%temp_out_directory%" /MIR
+@REM CALL :check-error "Failed to mirror copy output directory to deployment staging directory."
+ECHO.
 
 git checkout %deploy_branch%
 CALL :check-error "Failed to checkout deployment branch."
 
-robocopy "%temp_out_directory%" "%out_directory%" /MIR > nul
-CALL :check-error "Failed to mirror copy staging directory to deployment directory."
+robocopy "%temp_out_directory%" "%out_directory%" /MIR
+@REM CALL :check-error "Failed to mirror copy staging directory to deployment directory."
+ECHO.
 
 git add out/
 CALL :check-error "Failed to stage changes."
